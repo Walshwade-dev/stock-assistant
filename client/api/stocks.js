20cd7b64e6363471b2141c99c3160a467c2adefc
Walshwade-dev <mainace8@gmail.com>
2026-01-15 13:11:44 +0300

    Point frontend to deployed API

--- client/api/stocks.js
+++ client/api/stocks.js
@@ -1,19 +1,17 @@
+const API_URL = "https://stock-assistant-api.onrender.com";
+
 export async function fetchStock(tickers) {
-    const res = await fetch("http://localhost:3000/api/stocks", {
+    const res = await fetch(`${API_URL}/api/stocks`, {
         method: "POST",
         headers: {
-            "Content-Type": "application/json",
+            "Content-Type": "application/json"
         },
-        body: JSON.stringify({ tickers }),
+        body: JSON.stringify({ tickers })
     });
 
     if (!res.ok) {
         throw new Error("Backend error");
     }
 
-    const data = await res.json();
-    console.log(data.stocks);
-    console.log(data.report);
-
-    return data;
+    return res.json();
 }
